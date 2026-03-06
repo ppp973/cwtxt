f49885e5b4df51145c1b8cdfe173122f20ec870c
ppp973 <singhayush20368@gmail.com>
2026-03-06 15:57:05 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,19 +3,42 @@ Utils package for CareerWill Bot
 """
 
 from .api_helper import (
-    fetch_json, get_video_url, get_batch_info, get_topic_details,
-    get_all_batches, process_topic, extract_batch, validate_batch_id
+    fetch_json,
+    get_video_url,
+    get_batch_info,
+    get_topic_details,
+    get_all_batches,
+    process_topic,
+    extract_batch,
+    validate_batch_id,
+    check_api_status
 )
+
 from .file_helper import (
-    sanitize_filename, generate_filename, save_to_file,
-    read_from_file, cleanup_file, ensure_download_dir
+    sanitize_filename,
+    generate_filename,
+    save_to_file,
+    read_from_file,
+    cleanup_file,
+    ensure_download_dir
 )
 
 __all__ = [
-    'fetch_json', 'get_video_url', 'get_batch_info', 'get_topic_details',
-    'get_all_batches', 'process_topic', 'extract_batch', 'validate_batch_id',
-    'sanitize_filename', 'generate_filename', 'save_to_file',
-    'read_from_file', 'cleanup_file', 'ensure_download_dir'
+    'fetch_json',
+    'get_video_url',
+    'get_batch_info',
+    'get_topic_details',
+    'get_all_batches',
+    'process_topic',
+    'extract_batch',
+    'validate_batch_id',
+    'check_api_status',
+    'sanitize_filename',
+    'generate_filename',
+    'save_to_file',
+    'read_from_file',
+    'cleanup_file',
+    'ensure_download_dir'
 ]
 
 import logging
